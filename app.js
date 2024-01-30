368c26ad23be7dee1de296ce98293c5ee0a77ca9
kr007ritik <kumar007ritik@gmail.com>
2024-01-30 23:34:58 +0530

    checking answer and printing point of correct answer

--- app.js
+++ app.js
@@ -4,14 +4,18 @@ const nextBtn = document.querySelector(".nextBtn button");
 const questionNum = document.querySelector(".questionNum");
 const winnerMSg = document.querySelector(".winnerMSg");
 
-
 const arrOfQuestion = [
   ["National animal of INDIA?", "Cat", "Lion", "Tiger", "Cow"],
   ["National flower of INDIA?", "Lotus", "Rose", "Sunflower", "Lily"],
   ["National bird of INDIA?", "Eagle", "Peacock", "Pigeon", "Crow"],
   ["Capital of INDIA?", "Goa", "Mumbai", "Delhi", "Patna"],
+  ["Capital of Bihar?", "Gaya", "Patna", "Ranchi", "Kolkata"],
 ];
+const answerKey = ["Tiger", "Lotus", "Peacock", "Delhi","Patna"];
 
+const NumOfQues = arrOfQuestion.length;
+
+// Print questions
 for (let j = 1; j < arrOfQuestion[0].length; j++) {
   question.innerHTML = `Q. ${arrOfQuestion[0][0]}`;
   const li = document.createElement("li");
@@ -22,25 +26,42 @@ for (let j = 1; j < arrOfQuestion[0].length; j++) {
 }
 
 let activeQues = 0;
+
+questionNum.innerHTML = `${activeQues}/${NumOfQues}`;
 nextBtn.addEventListener("click", () => {
-    activeQues++;
-    if(activeQues >= arrOfQuestion.length){
-        console.log(activeQues);
-        question.innerHTML = "Result";
-        ul.innerHTML = "";
-        nextBtn.remove();
-        winnerMSg.style.display = "flex";
-    }else{
-        ul.innerHTML = "";
-        console.log(activeQues);
-        questionNum.innerHTML = `${activeQues + 1}/${arrOfQuestion.length}`;
-        question.innerHTML = `Q. ${arrOfQuestion[activeQues][0]}`;
-        for (let j = 1; j < arrOfQuestion[activeQues].length; j++) {
-            const li = document.createElement("li");
-            const button = document.createElement("button");
-            ul.appendChild(li);
-            li.appendChild(button);
-            button.innerHTML = arrOfQuestion[activeQues][j];
-        }
+  ul.style.pointerEvents = "auto";
+  activeQues++;
+  if (activeQues >= arrOfQuestion.length) {
+    question.innerHTML = "Result";
+    ul.innerHTML = "";
+    nextBtn.remove();
+    winnerMSg.style.display = "flex";
+    winnerMSg.innerHTML= `${NumOfCorrectAns} points out of ${NumOfQues}`
+  } else {
+    ul.innerHTML = "";
+    for (let j = 1; j < arrOfQuestion[activeQues].length; j++) {
+      question.innerHTML = `Q. ${arrOfQuestion[activeQues][0]}`;
+      const li = document.createElement("li");
+      const button = document.createElement("button");
+      ul.appendChild(li);
+      li.appendChild(button);
+      button.innerHTML = arrOfQuestion[activeQues][j];
     }
+  }
 });
+
+// Check Answer
+let NumOfCorrectAns = 0;
+
+function handleItemClick(event) {
+  let userAns = event.target.textContent;
+  if (userAns === answerKey[activeQues]) {
+    event.target.className = "correct";
+    NumOfCorrectAns++;
+  } else {
+    event.target.className = "wrong";
+  }
+  ul.style.pointerEvents = "none";
+}
+
+ul.addEventListener("click", handleItemClick);
